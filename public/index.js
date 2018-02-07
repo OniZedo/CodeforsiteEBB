4aa31de68cdd7fd82923629907beb7cc538d6b37
Eric Beau Breitenbach <ebbreitenbach@Erics-MacBook-Air.local>
2018-02-07 16:44:13 -0700

    messed around and got nowhere

--- public/index.js
+++ public/index.js
@@ -1,1 +1,11 @@
 console.log("js works");
+$(document).ready(function(){
+  $("button").click(function(){
+    console.log($("10"));
+      var div = $("10");
+      div.animate({height: '300px', opacity: '0.4'}, "slow");
+      div.animate({width: '300px', opacity: '0.8'}, "slow");
+      div.animate({height: '100px', opacity: '0.4'}, "slow");
+      div.animate({width: '100px', opacity: '0.8'}, "slow");
+  });
+});
